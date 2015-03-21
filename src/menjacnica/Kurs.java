814382e14ddcdeab367da8a50474f826637e77bc
MilanBrkicFON <milan.brkic1@yahoo.com>
2015-03-21 16:04:35 +0100

    dodata klasa Kurs(sadrzi atribute srednji, kupovni i prodajni) i povezana sa klasom Valute

---
+++ src/menjacnica/Kurs.java
@@ -0,0 +1,31 @@
+package menjacnica;
+
+public class Kurs {
+	private double prodajniKurs , srednjiKurs, kupovniKurs;
+
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	
+	
+}
