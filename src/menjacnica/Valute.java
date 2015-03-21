1ab778eec56667a2e935878d21ff36325a767af3
MilanBrkicFON <milan.brkic1@yahoo.com>
2015-03-21 16:00:14 +0100

    Napravljena klasa Valute sa atributima i set/get metodama

---
+++ src/menjacnica/Valute.java
@@ -0,0 +1,49 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Valute {
+
+	private String naziv , skraceniNaziv;
+	private double prodajniKurs , srednjiKurs, kupovniKurs;
+	private GregorianCalendar datum;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	
+	
+}
